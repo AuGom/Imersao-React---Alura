cb989e533c0b656ee1881d1da5de23442341da0e
Augusto Pedro Gomes <52221457+AuGom@users.noreply.github.com>
2020-07-30 19:44:15 -0300

    Partial categories form

--- mytoplist/src/pages/cadastro/Categoria/index.js
+++ mytoplist/src/pages/cadastro/Categoria/index.js
@@ -1,11 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import PageDefault from "../../../components/PageDefault";
 import { Link } from "react-router-dom";
 
 function CadastroCategoria() {
+  const valoresIniciais = { nome: "", descricao: "", cor: "#000" };
+
+  const [values, setValues] = useState(valoresIniciais);
+  const [categorias, setCategorias] = useState([]);
+
+  function handleValues(event) {
+    setValues(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setCategorias([...categorias, values]);
+  }
+
   return (
     <PageDefault>
-      <h1>cadarto Categoria</h1>
+      <h1>Cadastro da Categoria:{values.nome}</h1>
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            Nome da Categoria:
+            <input type="text" value={values.nome} onChange={handleValues} />
+          </label>
+        </div>
+        <div>
+          <label>
+            Dercrição:
+            <textarea
+              type="text"
+              value={values.descricao}
+              onChange={handleValues}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Cor:
+            <input type="color" value={values.cor} onChange={handleValues} />
+          </label>
+        </div>
+
+        <button>Cadastrar</button>
+      </form>
+
+      <ul>
+        {categorias.map((categoria, indice) => {
+          return <li key={`${categoria}${indice}`}>{categoria}</li>;
+        })}
+      </ul>
+
       <Link to="/">home</Link>
     </PageDefault>
   );
