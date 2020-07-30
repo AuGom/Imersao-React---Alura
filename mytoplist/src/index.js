afdcea2956cd3d33c0d4f2910ecc5f542e56ccfb
Augusto Pedro Gomes <52221457+AuGom@users.noreply.github.com>
2020-07-29 23:06:56 -0300

    Home page working

--- mytoplist/src/index.js
+++ mytoplist/src/index.js
@@ -1,20 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
 import "./index.css";
+
 import Home from "./pages/Home";
-
-import { BrowserRouter, Switch } from "react-router-dom";
-
 import CadastroVideo from "./pages/cadastro/Video";
 import CadastroCategoria from "./pages/cadastro/Categoria";
+
+const Pagina404 = () => <div>Página 404</div>;
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <route exact path="/" component={Home} />
-      <route path="/cadastro/video" component={CadastroVideo} />
-      <route path="/cadastro/categoria" component={CadastroCategoria} />
-      <route component={() => <div>404</div>} />
+      <Route exact path="/" component={Home} />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={Pagina404} />
     </Switch>
   </BrowserRouter>,
   document.getElementById("root")
